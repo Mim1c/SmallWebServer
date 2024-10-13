d3dd43c4d021a68b566da84a5ea5a652fef20871
Mim1c <mwbn_98msn.com>
2024-10-13 15:40:25 +0200

    Added Httplistener and dummy website

---
+++ SmallWebServer/SmallWebServer/Router.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallWebServer
+{
+    public class Router
+    {
+        public string WebsitePath { get; set; }
+
+        private Dictionary<string, ExtensionInfo> extFolderMap;
+
+        public Router() 
+        {
+            extFolderMap = new Dictionary<string, ExtensionInfo>()
+            {
+                {"ico", new ExtensionInfo() {Loader=ImageLoader, ContentType="image/ico"}},
+                  {"png", new ExtensionInfo() {Loader=ImageLoader, ContentType="image/png"}},
+                  {"jpg", new ExtensionInfo() {Loader=ImageLoader, ContentType="image/jpg"}},
+                  {"gif", new ExtensionInfo() {Loader=ImageLoader, ContentType="image/gif"}},
+                  {"bmp", new ExtensionInfo() {Loader=ImageLoader, ContentType="image/bmp"}},
+                  {"html", new ExtensionInfo() {Loader=PageLoader, ContentType="text/html"}},
+                  {"css", new ExtensionInfo() {Loader=FileLoader, ContentType="text/css"}},
+                  {"js", new ExtensionInfo() {Loader=FileLoader, ContentType="text/javascript"}},
+                  {"", new ExtensionInfo() {Loader=PageLoader, ContentType="text/html"}},
+            };
+        }
+        /// <summary>
+        /// Read in an image file and returns a ResponsePacket with the raw data.
+        /// </summary>
+        private ResponsePacket ImageLoader(string fullPath, string ext, ExtensionInfo extensionInfo)
+        {
+            FileStream fStream = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
+            BinaryReader br = new BinaryReader(fStream);
+            ResponsePacket ret = new ResponsePacket()
+            {
+                Data = br.ReadBytes((int)fStream.Length),
+                ContentType =
+                extensionInfo.ContentType
+            };
+            br.Close();
+            fStream.Close();
+
+            return ret;
+        }
+        /// <summary>
+        /// Read in what is basically a text file and return a ResponsePacket with the text UTF8 encoded.
+        /// </summary>
+        private ResponsePacket FileLoader(string fullPath, string ext, ExtensionInfo extInfo)
+        {
+            string text = File.ReadAllText(fullPath);
+            ResponsePacket ret = new ResponsePacket()
+            {
+                Data = Encoding.UTF8.GetBytes(text),
+                ContentType =
+                extInfo.ContentType,
+                Encoding = Encoding.UTF8
+            };
+
+            return ret;
+        }
+
+        private ResponsePacket PageLoader(string fullPath, string ext, ExtensionInfo extInfo)
+        {
+            ResponsePacket ret = new ResponsePacket();
+
+            if (fullPath == WebsitePath)
+            {
+                ret = Route(GET, "/index.html", null);
+            }
+            else
+            {
+                if (String.IsNullOrEmpty(ext))
+                {
+                    fullPath = fullPath + ".html";
+                }
+
+                fullPath = WebsitePath + "\\Pages";
+                ret = FileLoader(fullPath, ext, extInfo);
+            }
+            return ret;
+        }
+
+        public class ResponsePacket
+        {
+            public string Redirect { get; set; }
+            public byte[] Data { get; set; }
+            public string ContentType { get; set; }
+            public Encoding Encoding { get; set; }
+        }
+
+        internal class ExtensionInfo
+        {
+            public string ContentType { get; set; }
+            public Func<string,string,string, ExtensionInfo, ResponsePacket> Loader { get; set; }
+        }
+    }
+}
---
+++ SmallWebServer/SmallWebServer/Server.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Specialized;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+
+namespace SmallWebServer
+{
+    public static class Server
+    {
+        private static HttpListener listener;
+        private static Router router = new Router();
+        /// <summary>
+        /// Returns list of IP addresses assigned to localhost network devices, such as hardwired ethernet, wireless, etc.
+        /// </summary>
+        private static List<IPAddress> GetLocalHostIPs()
+        {
+            IPHostEntry host;
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            List<IPAddress> ips = host.AddressList.Where(ip => ip.AddressFamily ==
+            AddressFamily.InterNetwork).ToList();
+
+            return ips;
+        }
+
+        //Adds prefixes for the listener to be returned
+        private static HttpListener InitializeListener(List<IPAddress> localhostIPs) 
+        {
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add("http://localhost/");
+
+            localhostIPs.ForEach(ip =>
+            {
+                Console.WriteLine("Listening on IP " + ip.ToString() + "/");
+                listener.Prefixes.Add("http://" + ip.ToString() + "/");
+            });
+
+            return listener;
+        }
+
+        public static int maxSimultaneousConnections = 20;
+        private static Semaphore sem = new Semaphore(maxSimultaneousConnections, maxSimultaneousConnections);
+
+        //Start worker thread which listens for connections
+        //PORT: 80 HTTP - 433 HTTPS
+        private static void Start(HttpListener listener)
+        {
+            listener.Start();
+            Task.Run(() => RunServer(listener));
+        }
+
+        //Runs in seperate thread
+        private static void RunServer(HttpListener listener)
+        {
+            while (true)
+            {
+                sem.WaitOne();
+                StartConnectionListener(listener);
+            }
+        }
+
+        private static async void StartConnectionListener(HttpListener listener)
+        {
+            //Wait for connection
+            HttpListenerContext context = await listener.GetContextAsync();
+
+            //Release semaphore so other listeners can be started up
+            sem.Release();
+            Log(context.Request);
+
+            HttpListenerRequest request = context.Request;
+            //Refactor so only the path is considered and not parameters after the "?"
+            string path = request.Url.OriginalString;
+            string verb = request.HttpMethod;
+            NameValueCollection parms = request.QueryString;
+
+            router.Route(verb, path, parms);
+
+            
+
+            /*string response = @"<html><head><meta http-equiv='content-type' content='text/html; charset=utf-8'/>
+            </head><h1>Hello Browser!</h1></html> ";
+            byte[] encoded = Encoding.UTF8.GetBytes(response);
+            context.Response.ContentLength64 = encoded.Length;
+            context.Response.OutputStream.Write(encoded, 0, encoded.Length);
+            context.Response.OutputStream.Close();*/
+        }
+
+        public static void Start(string websitePath)
+        {
+            router.WebsitePath = websitePath;
+            List<IPAddress> localHostIPs = GetLocalHostIPs();
+            HttpListener listener = InitializeListener(localHostIPs);
+            Start(listener);
+        }
+
+        public static void Log(HttpListenerRequest request)
+        {
+            Console.WriteLine(request.RemoteEndPoint + " sends " + request.HttpMethod + " to /" + 
+                request.Url.AbsoluteUri);
+        }
+
+        public static string GetWebsitePath()
+        {
+            string websitePath = AppDomain.CurrentDomain.BaseDirectory;
+
+            return websitePath;
+        }
+    }
+}
